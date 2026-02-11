14247c006f89c7f530a66c761f0f5b058e5a5d48
unknown <jainkaushal899@gmail.com>
2026-02-11 20:38:40 +0530

    Add CSRF Origins

--- microtasks/settings.py
+++ microtasks/settings.py
@@ -34,6 +34,12 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://microtasks-api.onrender.com',  
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
+
 
 # Application definition
 
